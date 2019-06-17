62497608eacb64a85b56dd9175174a25aee77597
Conner Lindsley <connerlindsley@gmail.com>
2019-06-16 19:55:07 -0700

    Added small color shifting script for funsies. Working on PlayerPrefs

---
+++ PlayerPrefs_VersionControl_GlimmerTech/Assets/ColorShift.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ColorShift : MonoBehaviour
+{
+    public Color col1;
+    public Color col2;
+
+    public float speed = 3.0f;
+
+    private Image image;
+
+    public bool isTimerOn = false;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        while(isTimerOn)
+        {
+            float t = (Mathf.Sin(Time.time * speed) + 1) / 2.0f;
+            image.color = Color.Lerp(col1, col2, t);
+        }
+    }
+
+
+}
