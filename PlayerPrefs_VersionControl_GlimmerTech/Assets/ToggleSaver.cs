89cea2d91a4af3c7b46305d0d0a46e969f1a40c0
Conner Lindsley <connerlindsley@gmail.com>
2019-06-18 11:02:04 -0700

    Made the sound effect toggle tracked on player prefs

---
+++ PlayerPrefs_VersionControl_GlimmerTech/Assets/ToggleSaver.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ToggleSaver : MonoBehaviour
+{
+    private Toggle soundToggle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        soundToggle = GetComponent<Toggle>();
+
+        if(PlayerPrefs.GetInt("ToggleCheck") != 0)
+        {
+            if(PlayerPrefs.GetInt("ToggleCheck") == 1)
+            {
+                soundToggle.isOn = true;
+            }
+            else
+            {
+                soundToggle.isOn = false;
+            }
+        }
+    }
+
+    private void OnDisable()
+    {
+        if(soundToggle.isOn == true)
+        {
+            PlayerPrefs.SetInt("ToggleCheck", 1);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("ToggleCheck", 2);
+        }
+    }
+
+}
